be09af366405f02ff0386cf34433c45a849a898e
azanra <112457939+azanra@users.noreply.github.com>
2024-08-12 10:39:03 +0700

    Add function for basic math operators

---
+++ script.js
@@ -0,0 +1,17 @@
+let result = 0;
+
+function add(a, b){
+    return result = a + b;
+}
+
+function substract(a, b){
+    return result = a - b;
+}
+
+function multiply(a, b){
+    return result = a * b;
+}
+
+function divide(a, b){
+    return result = a / b;
+}
