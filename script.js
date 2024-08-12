c8298e2fca53a375f92f9d3741695f105c2d75b5
azanra <112457939+azanra@users.noreply.github.com>
2024-08-12 11:18:18 +0700

    Add function to decide which operation to do

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 let result = 0;
+let number = 0;
+let operator;
+let anotherNumber = 0;
 
-function add(a, b){
-    return result = a + b;
+function add(number, anotherNumber) {
+    return result = number + anotherNumber;
 }
 
-function substract(a, b){
-    return result = a - b;
+function substract(number, anotherNumber) {
+    return result = number - anotherNumber;
 }
 
-function multiply(a, b){
-    return result = a * b;
+function multiply(number, anotherNumber) {
+    return result = number * anotherNumber;
 }
 
-function divide(a, b){
-    return result = a / b;
+function divide(number, anotherNumber) {
+    return result = number / anotherNumber;
+}
+
+function operate(operator, number, anotherNumber) {
+    switch(operator) {
+        case '+':
+            add(number, anotherNumber);
+            break;
+        case '-':
+            substract(number, anotherNumber);
+            break;
+        case '*':
+            multiply(number, anotherNumber);
+            break;
+        case '/':
+            divide(number, anotherNumber);
+            break;
+    }
 }
